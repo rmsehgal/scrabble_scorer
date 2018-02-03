b9d702f5871ca80494f77edd0cbfce23f72ba086
rmsehgal <rmsehgal@gmail.com>
2018-02-03 17:29:18 +0000

    added unit test to check that board size is greater than zero

--- board.py
+++ board.py
@@ -3,6 +3,8 @@
 
 class Board(object):
     def __init__(self,size):
+        if size <= 0:
+            raise ValueError("Board size must be greater than 0")
         self.size = size
         self.board = [['.']*self.size]*self.size
 
--- board_test.py
+++ board_test.py
@@ -6,5 +6,9 @@ class BoardTest(unittest.TestCase):
         bb = board.Board(15)
         self.assertEqual(bb.sideLen(),15)
 
+    def test_board_size_is_negative(self):
+        with self.assertRaises(ValueError):
+            board.Board(-1)
+
 if __name__ == '__main__':
     unittest.main()

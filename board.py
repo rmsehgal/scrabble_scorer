baaaaa166e551687fde376340cfbfcb80479f501
Josh C <joshtron@gmail.com>
2018-02-03 09:31:42 -0800

    Josh added doc string, seeing if pre commit hook fires

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 class Board(object):
+    """Holds currently played tiles and values on special spots."""
     def __init__(self, size):
         self.size = size
         self.board = [['.'] * self.size] * self.size

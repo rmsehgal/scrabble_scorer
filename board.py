5d26f27bb637b6407c9f0814ae5342ed72b7abc8
Josh C <joshtron@gmail.com>
2018-02-03 09:11:34 -0800

    Forgot to git add board.py changes in last joshgc commit

--- board.py
+++ board.py
@@ -2,5 +2,10 @@
 
 
 class Board(object):
+    _size = 15
+
     def __init__(self):
-        self.board = [['.']*15]*15
+        self.board = [['.']*self._size]*self._size
+
+    def __len__(self):
+        return self._size

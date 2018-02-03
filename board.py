885463326b9647a35a78ccb52c64d18f2902a175
Josh C <joshtron@gmail.com>
2018-02-03 09:23:59 -0800

    Ran yapf with defaults on code

--- board.py
+++ board.py
@@ -1,11 +1,8 @@
-
-
-
 class Board(object):
     _size = 15
 
     def __init__(self):
-        self.board = [['.']*self._size]*self._size
+        self.board = [['.'] * self._size] * self._size
 
     def __len__(self):
         return self._size
--- board_test.py
+++ board_test.py
@@ -1,9 +1,11 @@
 import unittest
 import board
 
+
 class BoardTest(unittest.TestCase):
     def test_board_size_is_fifteen(self):
-        self.assertEqual(len(board.Board()),15)
+        self.assertEqual(len(board.Board()), 15)
+
 
 if __name__ == '__main__':
     unittest.main()

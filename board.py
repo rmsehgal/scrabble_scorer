a4f0b6bed996f206115b6607da9913e6713332af
rmsehgal <rmsehgal@gmail.com>
2018-02-14 18:40:56 +0000

    beggining work on word add function

--- board.py
+++ board.py
@@ -8,28 +8,51 @@ class Board(object):
         self.board = [['.'] * self.size] * self.size
 
 
-    def _checkBounds(self,wd_pos,direct):
+    def _checkBoundsPlace(self,wd_pos,direct):
+        if self.isEmpty:
+            mid_pos = [self.size//2]*2
+            if mid_pos not in wd_pos:
+                raise ValueError("First word placement must cross the center square of the board [%i,%i]"%(mid_pos[0],mid_pos[1]))
         if wd_pos[0][0] < 0 or wd_pos[0][1] < 0 or wd_pos[0][0] >= self.size or wd_pos[0][1] >= self.size:
             raise ValueError("Input position [%i,%i] is outside of board size"%(wd_pos[0][0],wd_pos[0][1]))
         if wd_pos[-1][0] >= self.size or wd_pos[-1][1] >= self.size:
             raise ValueError("Based on input position [%i,%i] word (%i squares %s) cannot fit on board"%(wd_pos[0][0],wd_pos[0][1],len(wd_pos),direct))
+
+    def _checkOverlap(self,wd_pos,word,direct):
+        if direct == 'down' or direct == 'd':
+            neighbor_mask = [[0,-1],[0,1]]
+        else:
+            neighbor_mask = [[-1,0],[1,0]]
+        isOL = False
+        for pp,cc in zip(wd_pos,word):
+            if self.board[pp[0]][pp[1]] != '.' and self.board[pp[0]][pp[1]] != cc:
+                raise ValueError("Overlaped character is wrong")
+            if self.board[pp[0]][pp[1]] != '.' and self.board[pp[0]][pp[1]] == cc:
+                isOL = True
+            for nm in neighbor_mask:
+                if self.board[pp[0]+nm[0]][pp[1]+nm[1]] != '.':
+                    #check that word formed from this locaiton is actualy a word
+                    isOL = self._checkNeighborWord()
+        return isOL
+    
+    def _checkNeighborWord(self):
+        return True
         
     def addWord(self,posit,direct,word):
-        if direct == 'down':
+        if direct not in ['down','up','d','u']:
+            raise ValueError('Direction must be one of down, d, up, or u input: %s'%direct)
+        if direct == 'down' or direct == 'd':
             wd_pos = [[posit[0]+ii,posit[1]] for ii in range(len(word))]
         else:
             wd_pos = [[posit[0],posit[1]+ii] for ii in range(len(word))]
-        self._checkBounds(wd_pos,direct)
-        if self.isEmpty:
-            #need to check that the word will be added to mid pt
-            mid_pos = [self.size//2]*2
-            if mid_pos not in wd_pos:
-                raise ValueError("First word placement must cross the center square of the board [%i,%i]"%(mid_pos[0],mid_pos[1]))
-            
-            for pp,cc in zip(wd_pos,word):
+        self._checkBoundsPlace(wd_pos,direct)
+        if not self.isEmpty:
+            self._checkOverlap(wd_pos,word,direct)
+        for pp,cc in zip(wd_pos,word):
+            if self.board[pp[0]][pp[1]] != '.':
                 self.board[pp[0]][pp[1]] = cc
-
-                
+    
+    
     
     def sideLen(self):
         return self.size
--- board_test.py
+++ board_test.py
@@ -7,11 +7,9 @@ class BoardTest(unittest.TestCase):
         bb = board.Board(15)
         self.assertEqual(bb.sideLen(), 15)
 
-
     def test_board_size_is_negative(self):
         with self.assertRaises(ValueError):
             board.Board(-1)
-
 
     def test_adding_first_word_to_board(self):
         bb = board.Board(5)
@@ -21,6 +19,14 @@ class BoardTest(unittest.TestCase):
         with self.assertRaises(ValueError):
             bb.addWord(pos,direct,wd)
 
+    def test_wrong_direction(self):
+        bb = board.Board(5)
+        pos = [0,0]
+        wd = 'aa'
+        direct = 'asdf'
+        with self.assertRaises(ValueError):
+            bb.addWord(pos,direct,wd)
+            
     def test_adding_word_outside_of_board(self):
         bb = board.Board(5)
         pos = [-1,2] 
